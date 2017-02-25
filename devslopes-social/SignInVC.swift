6b06c0452208587be71e066da2376087da4356c8
János Csizmadia <janos.csizmadia@attrecto.com>
2017-02-25 19:25:51 +0100

    Added Facebook and firebase Auth

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +22,62 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (
+            result, error) in
+            
+            if error != nil {
+                print("JANO: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                
+                print("JANO: User cancelled Facebook authentication")
+            } else {
+                
+                print("JANO: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    
+    
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            
+            if error != nil {
+                print("JANO: Unable to authenticate with Firebase - \(error)")
+            } else{
+                print("JANO: Successfully authenticated with Firebase")
+            }
+        })
+    }
+    
+    
+    
+    
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
