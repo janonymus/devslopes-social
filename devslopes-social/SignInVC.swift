a7933407e89ded03fce0335a7fad9a0a096cbfe3
János Csizmadia <janos.csizmadia@attrecto.com>
2017-02-25 19:43:14 +0100

    Added e-mail Auth

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -12,15 +12,18 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
     @IBAction func facebookBtnTapped(_ sender: Any) {
         
@@ -58,6 +61,29 @@ class SignInVC: UIViewController {
     }
     
     
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        // IF email password is 5 or less char long, the signin wont work
+        if let email = emailField.text, let pwd = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                
+                if error == nil {
+                    //The user existed, and the pw entered is correct
+                    print("JANO: Email user authenticated with firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        
+                        if error != nil {
+                            print("JANO: Unable to authenticate with firebase using email")
+                        } else {
+                            print("JANO: Successfully authenticated with Firebase using email")
+                        }
+                    })
+                }
+            })
+        }
+    }
     
     
     
