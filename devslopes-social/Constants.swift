58f58e50aa88e533d0d952a4680b15a1dab9a046
János Csizmadia <janos.csizmadia@attrecto.com>
2017-02-25 17:38:30 +0100

    Added custom views

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by János Csizmadia on 2/25/17.
+//  Copyright © 2017 János Csizmadia. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
