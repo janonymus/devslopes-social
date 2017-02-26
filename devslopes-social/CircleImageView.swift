03555d9b3b4553d01801e066552b19397425ab89
János Csizmadia <janos.csizmadia@attrecto.com>
2017-02-26 19:31:54 +0100

    Added UI to FeedVC

---
+++ devslopes-social/CircleImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImageView.swift
+//  devslopes-social
+//
+//  Created by János Csizmadia on 2/26/17.
+//  Copyright © 2017 János Csizmadia. All rights reserved.
+//
+
+import UIKit
+
+class CircleImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
